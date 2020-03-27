2575cc747955872170cfcf64ba48a987d2e77e3d
Season Knight <60442342+Season-Knight@users.noreply.github.com>
2020-03-27 14:15:07 -0600

    CRUD back-end

---
+++ HIking-api/react/precache-manifest.1b313005aac4c473cbbe7a689a6aee56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a11433a333b922d3b77b420f96ba6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7d5841d3d54a480a71",
+    "url": "/static/css/main.2adaae44.chunk.css"
+  },
+  {
+    "revision": "a0f79076c97c63e910f7",
+    "url": "/static/js/2.1bc58a5e.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/static/js/2.1bc58a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7d5841d3d54a480a71",
+    "url": "/static/js/main.5b3c518b.chunk.js"
+  },
+  {
+    "revision": "f452911b081f6079cf08",
+    "url": "/static/js/runtime-main.05e4ba61.js"
+  },
+  {
+    "revision": "4d230a52aca146e2f64b51e53f1b9760",
+    "url": "/static/media/MountainNight.4d230a52.jpg"
+  }
+]);
